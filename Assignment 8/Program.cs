40a6f4e44e93b8de09af5d31e11a5c06ce77e9e7
mariam-hamada <168047165+mariam-hamada@users.noreply.github.com>
2026-08-18 02:41:23 +0300

    Part 01 — Theoretical Questions Question 1

--- Assignment 8/Program.cs
+++ Assignment 8/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 — Theoretical Questions
+
+            #region Question 1
+
+            // a)  What is Abstraction in Object - Oriented Programming?
+            //hiding unnecessary implementation details and showing only the essential features of an object. It focuses on what an object does rather than how it does it.
+
+            // b)  Why is abstraction considered one of the four pillars of OOP ?
+            //helps reduce complexity, hide implementation details, improve code organization, and make programs easier to maintain and use.
+
+
+            #endregion
+
+            #endregion
         }
     }
 }
